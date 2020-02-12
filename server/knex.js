32b5b004b70563fb3c349ab0de1bdfd3bfcaa2a5
GentaShibasaki <genta.shibasaki.78@gmail.com>
2020-02-12 15:18:08 +0900

    wip

--- server/knex.js
+++ server/knex.js
@@ -5,10 +5,6 @@ const db = knex({
   connection:
     process.env.DATABASE_URL ||
     `postgres://${process.env.USER}@127.0.0.1:5432/solomvp`,
-  migrations: {
-    tablename: "knex_migrations",
-    directory: "./migrations"
-  },
   seeds: {
     directory: "./data"
   },
